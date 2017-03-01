48e702f4028fdde7a627c2fd73c9e9d03b3446ae
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-01 14:10:47 +0100

    Create a simple Rest Service from the Spring Tutorial #1

--- springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/App.java
+++ springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/App.java
@@ -1,13 +1,17 @@
 package de.chris.privateprojects.springRestMaven;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
