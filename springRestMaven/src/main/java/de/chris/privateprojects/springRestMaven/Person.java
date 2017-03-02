de7b043c00a8ac024dc2a59ec33a8030966ad997
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2017-03-02 12:26:07 +0100

    Extend the rest service to create, update, delete data #4

--- springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/Person.java
+++ springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/Person.java
@@ -19,6 +19,15 @@ public class Person {
 	@NotNull
 	private String lastName;
 	
+	
+	public Person(){
+		
+	}
+	public Person(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
 	public int getId() {
 		return id;
 	}
--- springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/RestControllerChris.java
+++ springRestMaven/src/main/java/de/chris/privateprojects/springRestMaven/RestControllerChris.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,14 +24,51 @@ public class RestControllerChris {
     
     @RequestMapping("/person")
     public Person getPerson(@RequestParam(value="id") Integer id){
-    	try{
-    		return personDao.findOne(id);
-    	}
-    	catch (Exception ex){
-    		//TODO: add a logger
-    		System.out.println("Person with the id=" +id +" not found");
-    		return null;
-    	}
-    	
+    		return personDao.findOne(id);	
     }
+    
+	@RequestMapping("/create")
+	public String create(String firstName, String lastName) {
+		String id = "";
+		try {
+			Person person = new Person(firstName, lastName);
+			personDao.save(person);
+			id = String.valueOf(person.getId());
+		} catch (Exception ex) {
+			return "Error creating the person: " + ex.toString();
+		}
+		return "Person succesfully created with id = " + id;
+	}
+	
+	@RequestMapping("/delete")
+	@ResponseBody
+	public String delete(int id){
+		try {
+			Person person = personDao.findOne(id);
+			personDao.delete(person);
+		} catch(Exception ex){
+			return "Error deleting the person: " + ex.toString();
+		}
+		return "Person succesfully deleted!";
+	}
+	
+	@RequestMapping("/update")
+	@ResponseBody
+	public String update(int id, String firstName, String lastName){
+		try {
+			Person person = personDao.findOne(id);
+			person.setFirstName(firstName);
+			person.setLastName(lastName);
+			personDao.save(person);
+		} catch (Exception ex){
+			return "Error updating the person: " +ex.toString();
+		}
+		return "Person succesfully updated!";
+	}
+	
+	
+	
+	
+	
+	
 }
